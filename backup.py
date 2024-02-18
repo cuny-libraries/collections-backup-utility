93562ff37ff59cd22dbea08737b3ff920df9152b
Mark Eaton <MarkEEaton@users.noreply.github.com>
2024-02-17 23:37:21 -0500

    add main function; test if directory exists

--- backup.py
+++ backup.py
@@ -10,15 +10,10 @@ COLLECTIONS = "https://api-na.hosted.exlibrisgroup.com/almaws/v1/bibs/collection
 BIBS_PARAMS = "/bibs?level=2&format=json&apikey="
 bibs_url = []
 
+
 load_dotenv()
 today = str(date.today())
-os.makedirs("data/" + today, exist_ok=True)
 
-with open("data/" + today + "/collections.json", "w") as f:
-    response = httpx.get(COLLECTIONS + os.getenv("APIKEY"))
-    data = response.json()
-    collections = json.dumps(data)
-    f.write(collections)
 
 def bibs(collections):
     for collection in collections["collection"]:
@@ -26,4 +21,27 @@ def bibs(collections):
             bibs(collection)
         bibs_url.append(collection["pid"]["link"] + BIBS_PARAMS + os.getenv("APIKEY"))
 
-bibs(data)
+
+def main():
+    try:
+        os.makedirs("data/" + today, exist_ok=False)
+    except FileExistsError:
+        print("Data for today already exists")
+        return
+
+    with open("data/" + today + "/collections.json", "w") as f1:
+        response = httpx.get(COLLECTIONS + os.getenv("APIKEY"))
+        data = response.json()
+        collections = json.dumps(data)
+        f1.write(collections)
+
+    bibs(data)
+
+    for index, url in enumerate(bibs_url):
+        with open("data/" + today + "/bibs" + str(index) + ".json", "w") as f2:
+            response = httpx.get(url)
+            data = response.json()
+            bib = json.dumps(data)
+            f2.write(bib)
+
+main()
