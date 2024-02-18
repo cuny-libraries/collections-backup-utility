9890ca4db714c315533e21e907926187b81fe93f
Mark Eaton <MarkEEaton@users.noreply.github.com>
2024-02-18 17:13:36 -0500

    basic pagination working

--- backup.py
+++ backup.py
@@ -7,56 +7,71 @@ from pprint import pprint
 
 
 COLLECTIONS = "https://api-na.hosted.exlibrisgroup.com/almaws/v1/bibs/collections?level=2&format=json&apikey="
-BIBS_PARAMS = "/bibs?level=2&format=json&limit=100&offset=5000&apikey="
-bibs_url = []
+BIBS_PARAMS = "/bibs?level=2&format=json&limit=100&apikey="
+TODAY = str(date.today())
 
 
 load_dotenv()
-today = str(date.today())
 
 
 def bibs(collections):
-    """ Recursively search collections and create urls """
+    """Recursively search collections and create urls"""
     for collection in collections["collection"]:
         if "collection" in collection:
             bibs(collection)
-        bibs_url.append(
-            (
-                collection["pid"]["link"] + BIBS_PARAMS + os.getenv("APIKEY"),
-                collection["name"],
-            )
+
+        counter = 0
+        json_output = []
+
+        return_data = paginate(collection, counter, json_output)
+
+        with open("data/" + TODAY + "/" + collection["name"] + ".json", "w") as f2:
+            bibs_json = json.dumps(return_data)
+            f2.write(bibs_json)
+
+
+def paginate(collection, counter, json_output):
+    """Paginate through results"""
+    if "pid" in collection:
+        url = (
+            collection["pid"]["link"]
+            + BIBS_PARAMS
+            + os.getenv("APIKEY")
+            + "&offset="
+            + str(counter)
         )
+        response = httpx.get(url, timeout=30)
+        data = response.json()
+        json_output.append(data)
+        counter += 100
+        if "bib" in data:
+            pprint(data["total_record_count"])
+            pprint(counter)
+            pprint("-------")
+            paginate(collection, counter, json_output)
+    else:
+        return json_output
 
 
 def main():
-    """ run the program """
+    """run the program"""
 
     # see if data for today already exists
     try:
-        os.makedirs("data/" + today, exist_ok=False)
+        os.makedirs("data/" + TODAY, exist_ok=False)
     except FileExistsError:
         print("Data for today already exists")
         return
 
     # get collections data
-    with open("data/" + today + "/collections.json", "w") as f1:
-        response = httpx.get(COLLECTIONS + os.getenv("APIKEY"))
+    with open("data/" + TODAY + "/collections.json", "w") as f1:
+        response = httpx.get(COLLECTIONS + os.getenv("APIKEY"), timeout=30)
         data = response.json()
         collections = json.dumps(data)
         f1.write(collections)
 
     # create urls for bibs data, save as tuples along with name
     bibs(data)
-
-    # create bibs files; populate from API
-    for index, url in enumerate(bibs_url):
-        with open(
-            "data/" + today + "/" + str(index) + " -- " + url[1] + ".json", "w"
-        ) as f2:
-            response = httpx.get(url[0])
-            data = response.json()
-            bibs_json = json.dumps(data)
-            f2.write(bibs_json)
 
 
 main()
